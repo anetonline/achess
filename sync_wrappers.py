fe68c5e4f08bb1951769637ebe59442acab73cbf
StingRay <jerry.reed@email.phoenix.edu>
2025-08-08 14:19:27 -0400

    Delete sync_wrappers.py

--- sync_wrappers.py
+++
@@ -1,8 +0,0 @@
-def print_text(*args, **kwargs):
-    print(' '.join(map(str, args)), **kwargs)
-
-# Override the global print function
-print = print_text
-
-# Example usage:
-# print('Hello, World!')  # This will now use print_text
